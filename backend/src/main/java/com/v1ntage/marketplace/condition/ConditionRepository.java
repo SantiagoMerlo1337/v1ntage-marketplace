6a7a34264146c5cf7d0490f681bb578d2a6cd73e
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-10-04 14:03:53 -0300

    remove condition

--- backend/src/main/java/com/v1ntage/marketplace/condition/ConditionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.v1ntage.marketplace.condition;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConditionRepository extends JpaRepository<Condition, Integer> {
-}
