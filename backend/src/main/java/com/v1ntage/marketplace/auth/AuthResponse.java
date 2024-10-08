45b9fa98d34d8e5fe991e1fd85b436a680eee9b1
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-10-08 16:47:32 -0300

    add JWT implementation

---
+++ backend/src/main/java/com/v1ntage/marketplace/auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.v1ntage.marketplace.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    String token;
+}
