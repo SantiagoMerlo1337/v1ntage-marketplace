2276f645a158badf39d0c2ffafd9898a7e31e078
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-10-08 16:37:46 -0300

    remove subcategory model

--- backend/src/main/java/com/v1ntage/marketplace/sub_category/SubCategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.v1ntage.marketplace.sub_category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubCategoryRepository extends JpaRepository<SubCategory, Integer> {
-}
